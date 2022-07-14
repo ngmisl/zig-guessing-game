4f81fe20c6095256354e4875e171a1b8ad2fb227
ngmisl <98217124+ngmisl@users.noreply.github.com>
2022-07-14 16:21:18 +0200

    works without random

--- guesser.zig
+++ guesser.zig
@@ -1,24 +1,27 @@
 const std = @import("std");
+const allocator = std.heap.page_allocator;
 
 pub fn main() !void {
-    var seed: u64 = undefined;
-    try std.os.getrandom(std.mem.asBytes(&seed));
+    // var seed: u64 = undefined;
+    //    try std.os.getrandom(std.mem.asBytes(&seed));
+    //
+    //    var prng = std.rand.DefaultPrng.init(seed);
+    //    const rand = &prng.random;
+    //
+    //    const target_number = rand.intRangeAtMost(u8, 1, 100);
 
-    var prng = std.rand.DefaultPrng.init(seed);
-    const rand = &prng.random;
-
-    const target_number = rand.intRangeAtMost(u8, 1, 100);
+    const target_number = 10;
 
     while (true) {
         const stdin = std.io.getStdIn().reader();
         const stdout = std.io.getStdOut().writer();
 
         const bare_line = try stdin.readUntilDelimiterAlloc(
-            std.heap.page_allocator,
+            allocator,
             '\n',
             8192,
         );
-        defer std.heap.page_allocator.free(bare_line);
+        defer allocator.free(bare_line);
 
         const guess = std.fmt.parseInt(u8, bare_line, 10) catch |err| switch (err) {
             error.Overflow => {
